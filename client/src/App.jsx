eb9df98a0452048a8fe05affe4726c61e373b08e
avinash <avinashhkumar99@gmail.com>
2024-10-12 14:23:34 +0530

    project initiated

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-8xl font-bold text-blue-600">
+        Hello, Tailwind with Vite!
+      </h1>
+    </div>
+  );
+};
+
+export default App;
