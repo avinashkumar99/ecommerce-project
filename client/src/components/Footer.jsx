235a57d48fa576687cd94c2d28250044702557aa
avinash <avinashhkumar99@gmail.com>
2024-10-14 23:48:01 +0530

    Footer created

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="w-full h-full flex justify-center bg-gray-950">
+      <div className="h-2/4">content comes here</div>
+    </div>
+  );
+};
+
+export default Footer;
