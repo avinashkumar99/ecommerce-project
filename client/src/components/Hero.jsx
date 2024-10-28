c7a195869c3052b8a36a01509247a7cec554f698
avinash <avinashhkumar99@gmail.com>
2024-10-28 19:47:46 +0530

    ui fixed for larger screen

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -5,23 +5,25 @@ const Hero = () => {
   return (
     <div className="w-screen h-screen flex border border-blue-400 flex-wrap md:flex-nowrap">
       {/* div for hero content */}
-      <div className="flex text-4xl h-full md:w-7/10">
-        <div className="flex flex-col w-full p-4 md:p-0 my-auto">
+      <div className="flex text-4xl h-full md:w-[60%]">
+        <div className="flex flex-col w-full p-4 md:p-0 my-auto 2xl:ms-20">
           {/* for showing offer part */}
-          <div className="text-center ">
-            <h3 className="2xl:text-2xl text-lg font-bold">
-              <span></span> Enjoy 50% OFF in Our Summer Super Sale!
+          <div className="text-center">
+            <h3 className="2xl:text-5xl text-lg font-bold 2xl:mb-5 mb-2">
+              <span className="rounded-3xl 2xl:px-12 2xl:py-4 bg-slate-100">
+                Enjoy 50% OFF in Our Summer Super Sale!
+              </span>
             </h3>
           </div>
           {/* some tags for hero section */}
           <div className="text-center my-4 ">
-            <h1 className="font-merriweather font-extrabold text-4xl">
+            <h1 className="font-merriweather font-extrabold text-4xl 2xl:text-8xl 2xl:my-12 2xl:tracking-wide">
               Step into Fashion at Your Ultimate Style Destination!
             </h1>
           </div>
           {/* some paragraphs */}
           <div className="text-center my-3">
-            <p className="font-crimson text-lg font-bold text-gray-400">
+            <p className="font-crimson text-lg font-bold text-gray-400 2xl:text-4xl 2xl:tracking-widest">
               Explore a World of Fashion Possibilities with Exclusive Discounts
               - Dive into Your Ultimate Style Destination and Elevate Your
               Wardrobe Today!
@@ -39,7 +41,7 @@ const Hero = () => {
         </div>
       </div>
       {/* div for image content */}
-      <div className="h-full md:w-3/10 w-full flex md:justify-end">
+      <div className="h-full md:w-[40%] w-full md:flex md:justify-end hidden lg:block">
         <div className="h-8/10 p-3 flex">
           <img className="" src="/images/heroImg1.png" />
         </div>
