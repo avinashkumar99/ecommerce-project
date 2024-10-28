0446b8f722c51fb94c9cfa9d9058539d44059279
avinash <avinashhkumar99@gmail.com>
2024-10-28 19:48:50 +0530

    button added

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Button = ({ name, iconName, color, textSize }) => {
   return (
     <div className="">
-      <button className="custom-button rounded-3xl ">
+      <button className="custom-button rounded-3xl 2xl:text-6xl 2xl:p-10 2xl:mt-16">
         {name} &nbsp;
         <i className={iconName} style={{ color: { color } }}></i>
       </button>
